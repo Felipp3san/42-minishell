73e2c54cab2fe5e3f65c7038b95e371531059e74
Felippe De Almeida Santana <fde-alme@c1r15s3.42porto.com>
2025-10-04 18:51:51 +0100

    env in progress

---
+++ tests/env/env_utils_test.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_utils_test.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fde-alme <fde-alme@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/04 17:01:22 by fde-alme          #+#    #+#             */
+/*   Updated: 2025/10/04 18:50:22 by fde-alme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "env.h"
+#include "libft.h"
+
+int	env_compare(char **env1, char **env2)
+{
+	int	i;
+
+	i = 0;
+	while (env1[i] && env2[i])
+	{
+		if (ft_strcmp(env1[i], env2[i]) != 0)
+			return (0); // mismatch
+		i++;
+	}
+	return (env1[i] == NULL && env2[i] == NULL);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_list	*list_clone;
+	char	**arr_clone;
+	char	*std_val;
+	char	*ft_val;
+
+	(void)argc;
+	(void)argv;
+	list_clone = env_clone(envp);
+
+	// Clone env as list
+	if (!list_clone)
+		ft_printf("env_clone failed\n");
+
+	// Arr to list
+	if (list_clone)
+	{
+		arr_clone = env_list_to_arr(list_clone);
+		if (env_compare(arr_clone, envp))
+			ft_printf("env_clone successfull!\n");
+		else
+			ft_printf("env_list_to_arr failed");
+		env_free(arr_clone);
+		ft_lstclear(&list_clone, free);
+	}
+
+	// Get env
+	std_val = getenv("PATH");
+	if (std_val)
+		ft_printf("%s\n", std_val);
+	ft_val = env_get(list_clone, "PATH");
+	if (ft_val)
+		ft_printf("%s\n", ft_val);
+
+	return (0);
+}
